172b83a98f38224ca0602fbfae879fcc1e305e08
John Monk <johntmonk1@gmail.com>
2023-02-13 17:45:31 -0600

    added appropriate code for mongoose schema and to connect to mongodb. Also provided logic for adding new entires via command line, and retrieving entries via command lone from database

---
+++ backend/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+if (process.argv.length < 3) {
+  console.log('give password as argument');
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://johnnymongodb:${password}@cluster0.dhhmrpz.mongodb.net/phonebookApp?retryWrites=true&w=majority`;
+
+mongoose.set('strictQuery', false);
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+const name = process.argv[3];
+const number = process.argv[4];
+
+if (!name && !number) {
+  console.log('phonebook:');
+  Person.find({}).then((res) => {
+    res.map(({ name, number }) => {
+      console.log(name, number);
+    });
+  });
+} else {
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+
+  person.save().then((res) => {
+    console.log(`Added ${name} number ${number} to phonebook.`);
+    mongoose.connection.close();
+  });
+}
+
+//How to use
+//In the command line enter "node mongo.js <password>" to retrieve list of names and nubmers in DB
+//To add an entry enter "node mongo.js <password> <new name> <new number>"
